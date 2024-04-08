1690a7d4123aea0866b49fc47639b3c2a55dae1b
3611071 <3611071@gmail.com>
2024-04-08 09:34:03 +0500

    08.04

---
+++ src/redux/data.js
@@ -0,0 +1,23 @@
+
+
+const albumData = [
+{id: '01', 
+title: '1',
+alt: 'Первая'},
+{id: '02', 
+title: '2',
+alt: 'Вторая'},
+{id: '03', 
+title: '3',
+alt: 'Третья'},
+{id: '04', 
+title: '4',
+alt: 'Четвертая'},
+{id: '05', 
+title: '5',
+alt: 'Пятая'},
+{id: '06', 
+title: '6',
+alt: 'Шестая'},
+];
+export {albumData}
